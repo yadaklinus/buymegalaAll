36c6c82b79cc68c327e0c89cc46a4871299c5cb2
muftimarket-ship-it <muftimarket@gmail.com>
2026-07-27 00:39:05 +0100

    CommandsUPdate

--- backend/auth/componets/login.js
+++ backend/auth/componets/login.js
@@ -37,7 +37,7 @@ const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
--- backend/auth/componets/register.js
+++ backend/auth/componets/register.js
@@ -64,7 +64,8 @@ const Register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
